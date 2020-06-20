f146a264cf17f52c25725af0e1423773ebaf5dce
Nathaniel Lally <nate.lally@gmail.com>
2020-06-20 00:35:15 -0400

    tagid

--- bin/events_summary.js
+++ bin/events_summary.js
@@ -69,20 +69,14 @@ console.trace = console.log
     let tweet = []
     let subject = []
 
-    log("here?")
     tids = t.keysByProfit('desc')
 
     let topN = tids.slice(0,5)
 
-    log("here?")
     topN.map(tid => ( subject.push(sprintf("%0.0f ",Math.round( t.profit(tid) / t.costBasis(tid) * 1000))) ))
-    log("here?")
 
-    log("here?")
     let top = tids.filter(tid => t.profit(tid) >= Number(opt.notify))
-    log("here?")
     top.map( tid => tweet.push(t.asTweet(tid) ))
-    log("not here")
     //        await rl.notify(tweet.join("\n"),subject.join(","))
 
     if (tweet.length > 0 ) {
@@ -103,6 +97,7 @@ console.trace = console.log
           promises.push(new Promise(async (resolve, resject) => {
             ev.created_by = 'events_pending'
             ev.transaction_tag = tid
+            ev.tagid = aid
 
             //  keep order book if one is embedded within
             //
