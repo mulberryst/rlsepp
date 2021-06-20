bf65f3a0bbbbf22f7ebceeb812f53447e3c493f1
NathanielLLally <Nathaniel.Lally@gmail.com>
2021-06-20 02:44:30 +0000

    socket test

--- tests/socket.js
+++ tests/socket.js
@@ -1,4 +1,8 @@
-const net = require('net');
+const net = require('net')
+,	WebSocket = require('ws')
+,	JSON = require('JSON')
+;
+
 const server = net.createServer((c) => {
   // 'connection' listener.
   console.log('client connected');
@@ -20,6 +24,39 @@ const server = net.createServer((c) => {
 server.on('error', (err) => {
   throw err;
 });
-server.listen(2324, () => {
+server.listen(2325, () => {
   console.log('server bound');
+});
+
+
+
+const wss = new WebSocket.Server({
+  port: 2324,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed.
+  }
+});
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send(JSON.stringify({'something': 1234}));
 });
